b7f6cc1d055e0997e89462334ba97487a5eb718a
Devdeep Patidar <patidardevdeep999@gmail.com>
2023-12-19 09:41:21 +0530

    thala for a reason

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [message, setMessage] = useState('');
+  const [showVideo, setShowVideo] = useState(false);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Calculate the sum of input values
+    const sum = inputValue
+      .split('')
+      .map(Number)
+      .reduce((acc, digit) => acc + digit, 0);
+
+    if (sum === 7) {
+      setMessage('Thala for a reason');
+      setShowVideo(true);
+    } else {
+      setMessage('');
+      setShowVideo(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Video App</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Input Value:
+          <input type="text" value={inputValue} onChange={handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {message && <p>{message}</p>}
+      {showVideo && (
+        <div>
+          <video width="560" height="315" autoPlay controls>
+            <source src="thala.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
